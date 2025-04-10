469f157c7ac4281eca3fbd5b89c88218d16d0f94
Nils Gämperli <n.gaemperli@gmail.com>
2025-04-10 22:07:08 +0200

    - normalize data before training - increased lr - added more layers in network - compute class weights

---
+++ ai_engineering/compute_class_weights.py
@@ -0,0 +1,7 @@
+import torch
+import numpy as np
+from sklearn.utils.class_weight import  compute_class_weight
+
+
+def compute_class_weights(labels:torch.Tensor):
+    return compute_class_weight(class_weight="balanced", classes=np.unique(labels), y=labels)
