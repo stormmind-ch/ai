61b53fdfe0d6606c9790c66d939fd12b10a65f10
Nils Gämperli <n.gaemperli@gmail.com>
2025-04-10 23:09:31 +0200

    fixed to float

--- ai_engineering/compute_class_weights.py
+++ ai_engineering/compute_class_weights.py
@@ -4,4 +4,5 @@ from sklearn.utils.class_weight import  compute_class_weight
 
 
 def compute_class_weights(labels:torch.Tensor):
-    return torch.from_numpy(compute_class_weight(class_weight="balanced", classes=np.unique(labels), y=labels))
+    weights = compute_class_weight(class_weight="balanced", classes=np.unique(labels), y=labels)
+    return torch.tensor(weights, dtype=torch.float32)
