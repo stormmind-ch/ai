16b39640ff363de9301d75ce66140d41c56909ff
Nils Gämperli <n.gaemperli@gmail.com>
2025-04-22 21:27:16 +0200

    updated visualize.py

--- ai_engineering/datasets/visualize.py
+++ ai_engineering/datasets/visualize.py
@@ -1,14 +1,13 @@
-import numpy as np
-from ClusteredStormDamageDataset import ClusteredStormDamageDataset
-
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import numpy as np
 import itertools
+from ClusteredStormDamageDataset import ClusteredStormDamageDataset
 
-def plot_scatter_matrix(dataset, feature_names=None, max_samples=500):
+def plot_scatter_matrix(dataset, feature_names=None, num_clusters = None):
     # Sample data
     X, y = [], []
-    for i in range(min(len(dataset), max_samples)):
+    for i in range(len(dataset)):
         features, label = dataset[i]
         X.append(features.numpy())
         y.append(label.item())
@@ -19,19 +18,47 @@ def plot_scatter_matrix(dataset, feature_names=None, max_samples=500):
     if feature_names is None:
         feature_names = [f'feat_{i}' for i in range(n_features)]
 
-    # Create grid with extra column for colorbar
-    fig = plt.figure(figsize=(13, 13))
+    # Setup figure with colorbar space
+    fig = plt.figure()
     gs = gridspec.GridSpec(n_features, n_features + 1, width_ratios=[1]*n_features + [0.05], wspace=0.1, hspace=0.1)
 
     axes = np.empty((n_features, n_features), dtype=object)
     scatter = None
+
+    mask_damage = y >= 1
+    mask_nodamage = y < 1
+
     for i, j in itertools.product(range(n_features), repeat=2):
         ax = fig.add_subplot(gs[i, j])
         axes[i, j] = ax
+
         if i == j:
-            ax.hist(X[:, i], bins=20, alpha=0.7)
+            ax.hist(X[:, i], bins=20, alpha=0.7, color='gray')
         else:
-            scatter = ax.scatter(X[:, j], X[:, i], c=y, cmap='viridis_r', s=10, alpha=0.7)
+            # Plot no-damage points
+            ax.scatter(
+                X[mask_nodamage][:, j],
+                X[mask_nodamage][:, i],
+                color='lightgray',
+                s=5,
+                alpha=0.3,
+                label='No Damage'
+            )
+
+            # Plot damage points
+            scatter = ax.scatter(
+                X[mask_damage][:, j],
+                X[mask_damage][:, i],
+                c=y[mask_damage],
+                cmap='viridis_r',
+                s=20,
+                alpha=0.9,
+                edgecolors='black',
+                linewidth=0.3,
+                label='Damage ≥ 1'
+            )
+
+        # Axis labels
         if i == n_features - 1:
             ax.set_xlabel(feature_names[j], fontsize=8)
         else:
@@ -41,22 +68,26 @@ def plot_scatter_matrix(dataset, feature_names=None, max_samples=500):
         else:
             ax.set_yticks([])
 
-    # Add colorbar in last column
-    cbar_ax = fig.add_subplot(gs[:, -1])
-    fig.colorbar(scatter, cax=cbar_ax, label="Damage")
+        # Add legend only once
+        if i == n_features - 1 and j == n_features - 1:
+            ax.legend(fontsize=6, loc='upper right')
 
-    fig.suptitle("Scatterplot Matrix of Weather Features vs. Damage", fontsize=14)
+    # Add shared colorbar
+    cbar_ax = fig.add_subplot(gs[:, -1])
+    fig.colorbar(scatter, cax=cbar_ax, label="Damage Severity")
+
+    fig.suptitle(f"Scatterplot Matrix of Weather Features vs. Damage, grouping: {dataset.timespan_calendar}, clusters: {num_clusters}", fontsize=10)
     plt.show()
 
 
 
-# Instantiate your dataset
+
 test_dataset = ClusteredStormDamageDataset('../../Ressources/main_data_1972_2023.csv',
                                            '../../Ressources/weather_data4',
                                            '../../Ressources/municipalities_coordinates_newest.csv',
-                                           'mean', 6, 'test', 4, 4,
+                                           'mean', 20, 'test', 4, 4,
                                            grouping_calendar='weekly',
                                            damage_weights={0: 0, 1: 0.06, 2: 0.8, 3: 11.3})
 
-plot_scatter_matrix(test_dataset, feature_names=['Temperature', 'Sunshine', 'Rain', 'Snow'])
+plot_scatter_matrix(test_dataset, feature_names=['Temperature', 'Sunshine', 'Rain', 'Snow'], num_clusters=20 )
 
